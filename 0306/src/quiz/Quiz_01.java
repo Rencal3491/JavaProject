182a8a1769b4b743700f9803d2f07ec5f5adfe4d
tjoeun <tjoeun@DESKTOP-9NUSN4M.mshome.net>
2024-03-07 17:12:22 +0900

    0307

---
+++ 0306/src/quiz/Quiz_01.java
@@ -0,0 +1,17 @@
+package quiz;
+
+public class Quiz_01 {
+
+	public static void main(String[] args) {
+		System.out.println("========= 출력결과 ==========");
+		System.out.println();
+		System.out.println("이름	: 홍길동");
+		System.out.println("나이	: 20");
+		System.out.println("Tel     : 010-1234-1234");
+		System.out.println("키  	: 178.5");
+		System.out.println("몸무게 	: 75");
+		System.out.println("혈액형 	: O");
+		
+	}
+
+}
