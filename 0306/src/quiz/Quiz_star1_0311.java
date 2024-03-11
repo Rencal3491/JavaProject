b65d0a63dc7532ede21c21e1eb2d4626a723667b
tjoeun <tjoeun@host.docker.internal>
2024-03-11 12:54:55 +0900

    0311

--- 0306/src/quiz/Quiz_star1_0311.java
+++ 0306/src/quiz/Quiz_star1_0311.java
@@ -8,7 +8,7 @@ public class Quiz_star1_0311 {
 		
 		for(int i=1; i<=5; i++) {
 			for(int j=1; j<=i; j++) {			
-			System.out.print("*");				
+				System.out.print("*");				
 			}
 			System.out.println();
 			
@@ -18,7 +18,7 @@ public class Quiz_star1_0311 {
 		
 		for(int i=1; i<=5; i++) {
 			for(int j=5; j>=i; j--) {			
-			System.out.print("*");				
+				System.out.print("*");				
 			}
 			System.out.println();
 		}
