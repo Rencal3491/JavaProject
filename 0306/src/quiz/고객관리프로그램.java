ed1dbb956791c9000eaefd8739d1fa486bb0f365
Rencal <Rencal@LAPTOP-95U0NBR6>
2024-03-12 17:59:02 +0900

    0312

---
+++ 0306/src/quiz/고객관리프로그램.java
@@ -0,0 +1,39 @@
+package quiz;
+
+import java.util.Scanner;
+
+public class 고객관리프로그램 {
+
+	public static void main(String[] args) {
+		String name;
+		String male;
+		String female;
+		String email;
+		int birth;
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+
+		switch (str) {
+		case "i","I": {			
+		}
+		case "p","P": {						
+		}			
+		case "u","U": {			
+		}	
+		case "d","D": {			
+		}
+		case "q","Q": {			
+		}
+			
+			
+					
+			
+			
+		default:
+			
+		}
+		
+		
+	}
+
+}
